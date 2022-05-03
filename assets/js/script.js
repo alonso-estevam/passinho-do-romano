45d8e43b6ad97c5039d173fd4ad2e5dfff18990b
alonso-estevam <alonso_estevam@outlook.com>
2022-05-02 21:32:20 -0300

    corrigido bug da função decodificar

--- assets/js/script.js
+++ assets/js/script.js
@@ -83,25 +83,27 @@ submit.addEventListener('click', function (event) {
         if (select.value == 1) {
             // pega a mensagem digitada
             var input = document.querySelector('#msg-input').value;
-            console.log(input);
+
             // pega o número de passos e converte para number
             var key = Number(document.querySelector('#keyBox').value);
-            console.log(key);
+            
             // converte a string em array
             var arr = input.split('');
             var arrCharCode = arr.map(function (item) {
                 // retorna o unicode dos itens do array
                 return item.charCodeAt()
             })
+            console.log(arrCharCode);
             var newArrCharCode = arrCharCode.map(function (item) {
                 if (item == 32) {
                     // trata a questão do unicode do espaço
                     return item = 32;
                 } else {
-                    //retorna o novo unicode após a soma dos passos
-                    return (((item - 97) - key) % 26) + 97
+                    //retorna o unicode decodificado
+                    return (((item - 97) - (key - 26)) % 26) + 97;
                 }
             })
+            console.log(newArrCharCode);
             var codedArr = newArrCharCode.map(function (item) {
                 // retorna o caractere correspondente ao novo unicode
                 return String.fromCharCode(item);
@@ -110,6 +112,7 @@ submit.addEventListener('click', function (event) {
             var codedMsg = document.querySelector('#msg-output');
             // mostra a mensagem codificada
             codedMsg.value = codedArr.join('');
+            
             // se for base64
         } else {
             // pega a mensagem codificada
